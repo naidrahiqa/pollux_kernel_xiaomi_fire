2f2ff0024c8f850263ef5a5e09be7170535b42db
naidrahiqa <akufaqih1@gmail.com>
2026-07-03 17:05:28 +0700

    Pollux: Migrate to ReSukiSU with manual hooks
    - Rename submodule path: KernelSU/ -> resukisu/
    - Update .gitmodules with ReSukiSU URL
    - Add drivers/kernelsu symlink targeting resukisu/kernel
    - Add KSU build entries in drivers/Makefile and Kconfig
    - Apply ReSukiSU manual hooks (exec, open, stat, reboot)
    - Export write_op + sel_handle_status_ops in selinuxfs.c
    - Add symlink setup step to CI workflow

--- fs/exec.c
+++ fs/exec.c
@@ -1917,8 +1917,17 @@ int do_execve(struct filename *filename,
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+#endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
+
+#ifdef CONFIG_KSU_MANUAL_HOOK
+__attribute__((hot))
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
+				void *argv, void *envp, int *flags);
+#endif
 
 int do_execveat(int fd, struct filename *filename,
 		const char __user *const __user *__argv,
@@ -1944,6 +1953,9 @@ static int compat_do_execve(struct filename *filename,
 		.is_compat = true,
 		.ptr.compat = __envp,
 	};
+#ifdef CONFIG_KSU_MANUAL_HOOK
+	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+#endif
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
